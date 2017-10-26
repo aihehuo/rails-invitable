bc1acbb3f25b6981a8fc5683f1bb712e8fb57abf
Bran Liang <lby89757@hotmail.com>
2017-10-26 15:00:34 +0800

    Add basic referral model test

--- test/models/rails_invitable/referral_test.rb
+++ test/models/rails_invitable/referral_test.rb
@@ -2,8 +2,22 @@ require 'test_helper'
 
 module RailsInvitable
   class ReferralTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "belongs to user" do
+      assert rails_invitable_referrals(:from_david).user
+    end
+
+    test "referral code is necessary" do
+      referral = RailsInvitable::Referral.create(user: users(:david))
+
+      assert_not referral.valid?
+    end
+
+    test "referral code can not be the same" do
+      code = '1234567890'
+      RailsInvitable::Referral.create(code: code, user: users(:david))
+      referral = RailsInvitable::Referral.create(code: code, user: users(:david))
+
+      assert_not referral.valid?
+    end
   end
 end
