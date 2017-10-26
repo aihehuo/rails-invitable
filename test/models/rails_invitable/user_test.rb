3864a9a228c0549780d2b8de742588ece56eaee1
Bran Liang <lby89757@hotmail.com>
2017-10-26 11:33:33 +0800

    Test the user class can be reached by the engine

--- test/models/rails_invitable/user_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module RailsInvitable
-  class UserTest < ActiveSupport::TestCase
-    test 'User model is linked from the host application' do
-      assert_kind_of Class, RailsInvitable.user_class
-    end
-  end
-end
--- test/rails_invitable_test.rb
+++ test/rails_invitable_test.rb
@@ -4,4 +4,8 @@ class RailsInvitable::Test < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, RailsInvitable
   end
+
+  test 'User model is linked from the host application' do
+    assert_kind_of Class, RailsInvitable.user_class
+  end
 end
