b840acd5f9c57dda5471beba2a3ee30077a14d13
Bran Liang <lby89757@hotmail.com>
2017-10-26 11:29:41 +0800

    Setup the test database migration

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20171026030136) do
 
+  create_table "rails_invitable_referrals", force: :cascade do |t|
+    t.string "code"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_rails_invitable_referrals_on_code"
+    t.index ["user_id"], name: "index_rails_invitable_referrals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
--- test/models/rails_invitable/user_test.rb
+++ test/models/rails_invitable/user_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 module RailsInvitable
   class UserTest < ActiveSupport::TestCase
-    test 'User model is linked from the host application'
+    test 'User model is linked from the host application' do
+      assert_kind_of Class, RailsInvitable.user_class
+    end
   end
 end
