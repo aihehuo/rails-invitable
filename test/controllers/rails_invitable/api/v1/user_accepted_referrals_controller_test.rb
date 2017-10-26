0747ae4039ada4e8ceb1885d7405bd2443655ddd
Bran Liang <lby89757@hotmail.com>
2017-10-26 16:53:39 +0800

    Fix the initiatial controller test

--- test/controllers/rails_invitable/api/v1/user_accepted_referrals_controller_test.rb
+++ test/controllers/rails_invitable/api/v1/user_accepted_referrals_controller_test.rb
@@ -5,12 +5,7 @@ module RailsInvitable
     include Engine.routes.url_helpers
 
     test "should get index" do
-      get api_v1_user_accepted_referrals_index_url
-      assert_response :success
-    end
-
-    test "should get create" do
-      get api_v1_user_accepted_referrals_create_url
+      get api_v1_user_accepted_referrals_url
       assert_response :success
     end
 
