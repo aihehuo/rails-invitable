1587c48b217e3f878a16f9dc54f7444e812c8c23
Bran Liang <lby89757@hotmail.com>
2017-10-30 19:12:18 +0800

    Add withdraw create api test

--- app/controllers/rails_invitable/api/v1/withdraws_controller.rb
+++ app/controllers/rails_invitable/api/v1/withdraws_controller.rb
@@ -3,8 +3,8 @@ require_dependency "rails_invitable/application_controller"
 module RailsInvitable
   class Api::V1::WithdrawsController < ApplicationController
     def create
-      authorize
-      withdraw = WithdrawsCreator.new(current_user, amount, channel).call
+      authorize Withdraw
+      withdraw = WithdrawCreator.new(current_user, params[:amount].to_f).call
       if withdraw
         render json: { data: withdraw.transfer, channel: withdraw.channel }.to_json, status: :created
       else
--- test/controllers/rails_invitable/api/v1/withdraws_controller_test.rb
+++ test/controllers/rails_invitable/api/v1/withdraws_controller_test.rb
@@ -4,8 +4,14 @@ module RailsInvitable
   class Api::V1::WithdrawsControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
-    # test "the truth" do
-    #   assert true
-    # end
+    setup do
+      ApplicationController.any_instance.stubs(:current_user).returns(users(:david))
+      Pingpp::Transfer.expects(:create).with(Not(equals(nil))).at_least(0).returns({mocha: true})
+    end
+
+    test "create is success" do
+      post api_v1_withdraws_url, params: { amount: 10 }
+      assert_response :success
+    end
   end
 end
