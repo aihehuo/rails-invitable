f5d9f12281a6397927a6aeb4cd6acba0aec42765
Bran Liang <lby89757@hotmail.com>
2017-10-30 12:15:50 +0800

    Add uniqness restrict on red pocket records

--- app/models/rails_invitable/red_pocket_record.rb
+++ app/models/rails_invitable/red_pocket_record.rb
@@ -2,5 +2,8 @@ module RailsInvitable
   class RedPocketRecord < ApplicationRecord
     belongs_to :referable, polymorphic: true
     belongs_to :user, class_name: RailsInvitable.user_class.to_s
+
+    validates :referable_id, :referable_type, presence: true
+    validates :referable_id, uniqueness: { scope: :referable_type }
   end
 end
--- test/models/rails_invitable/red_pocket_record_test.rb
+++ test/models/rails_invitable/red_pocket_record_test.rb
@@ -9,5 +9,12 @@ module RailsInvitable
     test "belongs_to referable" do
       assert rails_invitable_red_pocket_records(:from_david).referable
     end
+
+    test "referable is uniq" do
+      implemented_referable = RailsInvitable::RedPocketRecord.first.referable
+      record = RailsInvitable::RedPocketRecord.create(user: users(:yuan), amount: 10, referable: implemented_referable)
+
+      assert_not record.valid?
+    end
   end
 end
