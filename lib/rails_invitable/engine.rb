128abe258bd343ef23df4a6a64380394f966b99c
Bran Liang <lby89757@hotmail.com>
2017-10-30 15:47:19 +0800

    Add pingpp depency

--- lib/rails_invitable/engine.rb
+++ lib/rails_invitable/engine.rb
@@ -8,3 +8,11 @@ module RailsInvitable
     config.generators.api_only = true
   end
 end
+
+# Pingpp is needed for the payment implementation.
+begin
+  require 'pingpp'
+rescue LoadError
+  puts "Please add the pingpp gem to your application's Gemfile. The rails_invitable engine pingpp for payment."
+  exit
+end
