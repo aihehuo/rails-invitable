1a46766a2222fbe42e0145213906edf57797eb7f
Bran Liang <lby89757@hotmail.com>
2017-10-31 11:48:13 +0800

    Add referral_complete_red_pocket_reward configuration

--- lib/rails_invitable/configuration.rb
+++ lib/rails_invitable/configuration.rb
@@ -3,12 +3,14 @@ module RailsInvitable
     attr_accessor :allowed_withdraw_channels,
                   :minimum_withdraw_amount,
                   :order_prefix,
-                  :pingpp_app_id
+                  :pingpp_app_id,
+                  :referral_complete_red_pocket_reward
 
     def initialize
       @allowed_withdraw_channels = ['wx_pub', 'alipay']
       @minimum_withdraw_amount = 10
       @order_prefix = 'HEPBURNORDER'
+      @referral_complete_red_pocket_reward = 5
     end
   end
 end
