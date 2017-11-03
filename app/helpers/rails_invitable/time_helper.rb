7686467ee57c142fedbb41411b7942ea1c6f7a0c
Bran Liang <lby89757@hotmail.com>
2017-11-03 16:53:20 +0800

    Use formated created_at

--- app/helpers/rails_invitable/time_helper.rb
+++ app/helpers/rails_invitable/time_helper.rb
@@ -3,5 +3,9 @@ module RailsInvitable
     def format_to_date(time)
       time.strftime("%Y-%m-%d")
     end
+
+    def format_to_datetime(time)
+      time.strftime("%Y-%m-%d %H:%M:%S")
+    end
   end
 end
