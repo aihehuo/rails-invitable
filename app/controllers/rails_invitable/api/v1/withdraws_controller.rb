730aa7496390fdf36b322edf7d3026b4382e024d
Bran Liang <lby89757@hotmail.com>
2017-11-08 11:15:36 +0800

    Render error message on withdraw fail

--- app/controllers/rails_invitable/api/v1/withdraws_controller.rb
+++ app/controllers/rails_invitable/api/v1/withdraws_controller.rb
@@ -8,14 +8,14 @@ module RailsInvitable
       if withdraw
         render json: { data: withdraw.transfer, channel: withdraw.channel }.to_json, status: :created
       else
-        head 422
+        render json: { error: { message: '提现失败' } }.to_json, status: 422
       end
     end
 
     def succeed
       unless RailsInvitable.configuration.webhook_ip_whitelist.include? env["action_dispatch.remote_ip"].to_s
         Rails.logger.info "#{RailsInvitable.configuration.webhook_ip_whitelist} does not have #{env["action_dispatch.remote_ip"].to_s}"
-        return 
+        return
       end
       if params[:type] == 'transfer.succeeded'
         Rails.logger.info "Calling withdraw succeed #{params[:data][:object][:order_no]}"
